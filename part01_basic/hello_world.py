c4d29887030d92239da56b61c70fa5a6c0a77c22
lilacqueen <93392701+ApprenticeOne@users.noreply.github.com>
2021-11-01 20:31:27 +0000

    update

--- part01_basic/hello_world.py
+++ part01_basic/hello_world.py
@@ -1,3 +1,5 @@
+import sys
+
 print('hello, world!')
 print("你好,世界！")
 print('你好', '世界')
@@ -9,11 +11,12 @@ help(print)
         输出对象到流，多个对象需要分隔符
         
         Optional keyword arguments:
-            file:  a file-like object (stream); defaults to the current sys.stdout. 写入文件对象
+            file:  a file-like object (stream); defaults to the current sys.stdout. 写入文件对象???
             sep:   string inserted between values, default a space. 分隔符 默认空格
             end:   string appended after the last value, default a newline. 结尾 默认'\n'
-            flush: whether to forcibly flush the stream. 输出是否被缓存通常决定于 file，但如果 flush 关键字参数为 True，流会被强制刷新。
-        
+            flush: whether to forcibly flush the stream. 输出是否被缓存通常决定于 file，但如果 flush 关键字参数为 True，流会被强制刷新。我不理解
 '''
-print('hello', 'world', sep=', ', end='!') # 不存在'\n' 结尾不换行
+open('test.txt', 'w')  # 只能写
+print('hello', file=sys.stdout)  # 我不理解
+print('hello', 'world', sep=', ', end='!')  # 不存在'\n' 结尾不换行
 print('goodbye, world', end='!\n')
