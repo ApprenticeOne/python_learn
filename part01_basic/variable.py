dbdd78f7789ca077dfdd00f3e1338e720eb26116
lilacqueen <93392701+ApprenticeOne@users.noreply.github.com>
2021-11-02 20:43:19 +0000

    update

--- part01_basic/variable.py
+++ part01_basic/variable.py
@@ -37,3 +37,15 @@ print('flag5 =', flag5)    # flag5 = False
 # 获取输入 类型转换
 a = int(input('a = '))
 b = int(input('b = '))
+# 格式化输入
+print('%d + %d = %d' % (a, b, a + b))
+
+"""
+输入年份 如果是闰年输出True 否则输出False
+"""
+
+year = int(input('请输入年份: '))
+# 如果代码太长写成一行不便于阅读 可以使用\对代码进行折行
+is_leap = year % 4 == 0 and year % 100 != 0 or \
+          year % 400 == 0
+print(is_leap)
