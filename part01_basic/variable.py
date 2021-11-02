2be793cc112513d6cb2ef3c1a33d0dd47c236900
lilacqueen <93392701+ApprenticeOne@users.noreply.github.com>
2021-11-02 20:36:16 +0000

    update

---
+++ part01_basic/variable.py
@@ -0,0 +1,17 @@
+a = 321
+b = 12
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+a = 100
+b = 12.345
+c = 1 + 5j  # 这个j是个啥意思
+d = 'hello, world'
+e = True
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'complex'> 复数
+print(type(d))  # <class 'str'>
+print(type(e))  # <class 'bool'>
